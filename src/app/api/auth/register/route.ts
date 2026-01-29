f5f91c95aed254bb09033e6e94ad2db31f3d8638
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2026-01-29 13:18:02 +0530

    defined route

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,31 @@
+import { prisma } from "@/lib/prisma";
+import { hashPassword } from "@/lib/hash";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { name, email, password } = await req.json();
+
+  if (!name || !email || !password) {
+    return NextResponse.json({ error: "All fields required" }, { status: 400 });
+  }
+
+  const existingUser = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) {
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+  }
+
+  const hashedPassword = await hashPassword(password);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return NextResponse.json({ message: "User created" });
+}
